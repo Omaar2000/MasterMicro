d0761e6fdcaeda1598aa06ecf7ec5e2faf5edafe
Omar Mostafa <79061844+Omaar2000@users.noreply.github.com>
2022-05-23 22:50:42 +0200

    Update main.cpp

--- flatten 3D array/main.cpp
+++ flatten 3D array/main.cpp
@@ -7,6 +7,7 @@ int main()
     int arr[MAX][MAX][MAX];
     int idx=0,d=0,r=0,c=0;
     int ch=0,d1=0,d2=0,d3=0;
+    cout<<" Enter '1' for 1D array OR '3' for 3D array OR any number to exit!"<<endl;
     cin>>ch;
     cout<<" enter dimensions"<<endl;
     cin>>d1>>d2>>d3;
@@ -19,7 +20,7 @@ int main()
             idx=d*d2*d3+ r*d3+ c;
             cout<<idx<<endl;
         }
-        else
+        else if(ch==3)
         {    cout<<"enter index to be converted to 3D"<<endl;
             cin>>idx;
             d=idx/(d2*d3);
@@ -27,6 +28,9 @@ int main()
             c=(idx-d*(d2*d3))-r*d3;
             cout<<d<<" "<<r<<" "<<c<<endl;
         }
+        else break;
+        
+    cout<< " Enter '1' for 1D array OR '3' for 3D array OR any number to exit!"<<endl;
         cin>>ch;
     }
     return 0;
